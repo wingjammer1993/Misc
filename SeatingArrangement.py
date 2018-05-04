f789dbed67840e5439867127361b1457b75b073a
amruta <hail.radical@gmail.com>
2018-05-04 13:52:40 -0600

    seating

---
+++ SeatingArrangement.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+def create_random_seating(section_map_dict, room_map_dict):
+	seating_map = {}
+	for section in section_list:
+
+
+
+
+
+if __name__ == "__main__":
+
+	sections_map = { 101: 25, 102: 25, 103: 19, 104: 23, 105: 25, 106: 26,
+	                 107: 26, 108: 25, 109: 25, 110: 25, 111:26, 112: 25 }
+
+	room_map = {'A': 12, 'B': 22, 'C': 24, 'D': 25,
+	             'E': 26, 'F': 26, 'G': 29,'H': 29,
+	             'I': 30, 'J': 33, 'K': 33,'L': 34,
+	             'M': 37, 'N':37 ,'O': 28}
+	create_random_seating(sections_map, room_map)
+
+
+
+
